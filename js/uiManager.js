3af5b72acd6ff744b72806ce507bfc33f592851c
Arthur Jamain <arthur.jamain@gmail.com>
2012-06-27 16:45:15 +0200

    wtf debugging

--- js/uiManager.js
+++ js/uiManager.js
@@ -233,6 +233,7 @@ function($, _, views) {
     **/
     setGlobalConfig: function(conf) {
       console.log(conf);
+      alert("ok");
       if(conf && conf.app && conf.template) {
         if(conf.app.name) 
           $('title').html(conf.app.name);
